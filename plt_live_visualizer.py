005e8ad8020b5f43158515f8c0529f125c7051b4
Fabio Rose <rosef@hochschule-trier.de>
2024-08-21 15:15:07 +0200

    fixed size of figure window + added more comments

--- plt_live_visualizer.py
+++ plt_live_visualizer.py
@@ -1,16 +1,24 @@
 import nidaqmx
-import matplotlib
 import matplotlib.pyplot as plt
 import math
+import nidaqmx.constants
 import numpy as np
 
-print(matplotlib.get_backend())
 plt.ion() 
 
-SAMPLING_RATE = 10000
-SAMPLES_TO_READ = 1000
-# list of input channels to be read -> ["ai1"] / ["ai1", "ai2", "ai3"]
-CHANNELS = ["ai1", "ai2"]
+"""
+WARNING:
+Depending on the specified sampling rate and number of samples to read the plotting might crash because the GUI backend won't
+handle the rendering. Default settings (sampling_rate = 1000, samples_to_read = 500) are proven to work for up to 8 channel simultaneously, resulting 
+in a frame rate of 2 FPS.
+"""
+# Number of samples per second read by the DAQ board
+SAMPLING_RATE = 1000
+# number of samples to read per request (via task.read())
+SAMPLES_TO_READ = 500
+# list of input channels to be read -> e.g. ["ai1"] or ["ai1", "ai2", "ai3"]
+# up to 8 input channels can be plotted simultaneusly
+CHANNELS = ["ai0","ai1", "ai2", "ai3", "ai4", "ai5", "ai6","ai7"]
 
 def create_subplots(num_subplots: int, max_per_row: int = 4) -> list:
     """ Creates a list of <num_subplots> matplotlib axes objects. Subplots will be created so that if possible 
@@ -46,6 +54,11 @@ def create_subplots(num_subplots: int, max_per_row: int = 4) -> list:
     return axs
 
 
+##################################################################################################################
+#                                                                                                                #
+#                                                   DAQ Task Creation                                            #
+#                                                                                                                #
+##################################################################################################################
 print("creating task")
 in_task = nidaqmx.task.Task(new_task_name="AcquisitionTask")
 
@@ -62,16 +75,20 @@ in_task.timing.cfg_samp_clk_timing(
 
 in_task.start()
 
+
+
 # Creating matplotlib figure with subplots
-fig = plt.figure(figsize=(15,10))
+if len(CHANNELS) <= 4:
+    fig = plt.figure(figsize=(19,5))
+else:
+    fig = plt.figure(figsize=(19,11))
 fig.canvas.manager.set_window_title("DAQ-Board Visualizer")
 axs = create_subplots(num_subplots=len(CHANNELS))
 
-# change x-axis tick labels
-x = np.linspace(0,1000,10)
+# change x-axis tick labels to display time steps
+x = np.linspace(0,SAMPLES_TO_READ,5)
 time_labels = np.linspace(-SAMPLES_TO_READ/SAMPLING_RATE, 0, len(x))
 time_labels_str = [f"{t:.4f}" for t in time_labels]
-print(time_labels_str)
 plt.setp(axs, xticks=x, xticklabels=time_labels_str)
 
 # correctly title each subplot after corresponding channel and initialize a line plot
@@ -86,19 +103,32 @@ for i, ax in enumerate(axs):
  
 try:
     while True:
+        # READING DAQ data
         if in_task.in_stream.avail_samp_per_chan == 0:
             continue
-        # will return a list of values if one channel is selected, otherwise a multidimensional list of values for each channel
-        data = in_task.read(number_of_samples_per_channel=1000)
+        """
+        task.read()
+        If one channel is selected it will return a list of values for this specific channel
+        If multiple channels are selected it will return a multidimensional array where each dimension contains values for specific channel
+        
+        number_of_samples_per_channel
+        In this case a specific amount of data will be read from the internal buffer. By setting
+        number_of_samples_per_channel=nidaqmx.constants.READ_ALL_AVAILABLE all available data will be read from the internal buffer.
+        More information: https://nidaqmx-python.readthedocs.io/en/stable/task.html#nidaqmx.task.InStream.read 
+        """
+        data = in_task.read(number_of_samples_per_channel=SAMPLES_TO_READ)
+        
+        # updating plots with new data
         if len(CHANNELS) > 1:
             for i, line in enumerate(lines):
                 line.set_ydata(data[i])
                 axs[i].set_ylim([min(data[i]), max(data[i])])
-                axs[i].set_xlim([0, 1000])
+                axs[i].set_xlim([0, SAMPLES_TO_READ])
         else:
             lines[0].set_ydata(data)
             axs[0].set_ylim([min(data), max(data)])
-            axs[0].set_xlim([0, 1000])
+            axs[0].set_xlim([0, SAMPLES_TO_READ])
+        fig.tight_layout()
         fig.canvas.draw() 
         fig.canvas.flush_events()
 except KeyboardInterrupt:
